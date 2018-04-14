1498ea32416c3dccbf14753464307524e399bd7f
Tristanieto <tristan.nietomarquina.y7264@student.hogent.be>
2018-04-14 14:05:52 +0200

    Live filter

--- TaakWebapps/src/app/app.component.ts
+++ TaakWebapps/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PostDataService } from './post-data.service';
 import { Post } from './post/post.model';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   providers: [PostDataService],
@@ -11,9 +12,10 @@ import { Post } from './post/post.model';
 export class AppComponent {
 
   private filterPostName: string;
+  private filterPost$ = new Subject<string>();
   
 constructor(private _postDataService : PostDataService){
-
+  this.filterPost$.subscribe(val => this.filterPostName = val);
 }
 
 get posts(): Post[]{
